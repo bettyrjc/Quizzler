2d18a94a2d2e9ff746d3a8f59e8c07a0e6e2b42e
Betty Jimenez <bettyjimenez3010@gmail.com>
2023-03-22 11:25:10 -0400

    :sparkles: introduce a view for choice answers

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var choiceOneButton: UIButton!
+    @IBOutlet weak var ChoiceSecondButton: UIButton!
+    @IBOutlet weak var choiceThreeButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var falseButton: UIButton!
+
+    @IBOutlet weak var rightAnswer: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     var quizBrain = QuizBrain()
 
+    
     @IBOutlet weak var labelScore: UILabel!
     
     
@@ -31,25 +35,37 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer = sender.currentTitle
         let  userGotItRight = quizBrain.checkAnswer(userAnswer!)
-    
+        let correctAnswer = quizBrain.whichIsCorrectAnswer()
         
         if userGotItRight {
-            sender.backgroundColor = UIColor.green
+           sender.backgroundColor = UIColor.green
+            rightAnswer.text = "Great is the correct answer!"
         }else{
+            rightAnswer.text = "The correct answer was: \(correctAnswer)"
             sender.backgroundColor = UIColor.red
         }
         
         quizBrain.nextQuestion()
-        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
       
     }
     
     @objc func updateUI(){
         questionLabel.text = quizBrain.getQuestionText()
+        let answerChoices = quizBrain.getAnswers()
+        
+        choiceOneButton.setTitle(answerChoices[0], for: .normal)
+        ChoiceSecondButton.setTitle(answerChoices[1], for: .normal)
+        choiceThreeButton.setTitle(answerChoices[2], for: .normal)
+        
         progressBar.progress = quizBrain.getProgress()
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
         labelScore.text = "Score: \(quizBrain.getStore())"
+        rightAnswer.text = "Waiting your answer..."
+        
+        choiceOneButton.backgroundColor = UIColor.clear
+        ChoiceSecondButton.backgroundColor = UIColor.clear
+        choiceThreeButton.backgroundColor = UIColor.clear
+       
     }
 }
 
